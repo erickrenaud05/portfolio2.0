0f98bd34808380ac9274e25b076e15e2e1e4c28b
Erick Renaud <renaud_junior@outlook.com>
2024-09-27 00:43:17 -0400

    Added about me

--- portfolio2.0/src/components/About.jsx
+++ portfolio2.0/src/components/About.jsx
@@ -1,7 +1,16 @@
+import './About.css'
+import Avatar from '../assets/myAvatar.png'
+
 function About(){
     return (
-        <h3>About me</h3>
-    )
+        <div>
+            <h3>About Me</h3>
+            <img src={Avatar} alt="My avatar"></img>
+            <p>
+                Hi, my name is Erick. I'm a fullstack developer with a passion for learning.
+            </p>
+        </div>
+)
 }
 
 export default About
